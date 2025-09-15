cfe142750eff3875e7296a4167e8c850093c10c5
email <https://github.com/lunafroilan02-crypto/jecel21.git>
2025-09-15 15:15:18 +0800

    Updated project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jecel Payahay
         </a>
       </header>
     </div>
